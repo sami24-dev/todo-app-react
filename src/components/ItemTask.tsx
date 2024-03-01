e0d44a97b0de08959abc4a4bc371d3357819fcb6
samir <samirgm.dev@gmail.com>
2024-03-01 18:36:31 -0500

    add class button

--- src/components/ItemTask.tsx
+++ src/components/ItemTask.tsx
@@ -1,15 +1,15 @@
+import { useTodo } from '../context/TaskContext'
+import Check from '../images/cheque1.png'
 import { Task } from '../model/Model'
 import { IconDelete } from './Icons'
-import Check from '../images/cheque1.png'
-import { useTodo } from '../context/TaskContext'
 
-function ItemTask({task}:{task:Task}) {
-  const {  removeTask, toggleTask } = useTodo()
+function ItemTask({ task }: { task: Task }) {
+  const { removeTask, toggleTask } = useTodo()
   return (
     <li
       key={task.id}
       className='task'>
-      <div className='flex items-center gap-x-5'>
+      <div className='flex items-center gap-x-5 text-base md:text-xl capitalize'>
         <button onClick={() => toggleTask(task.id)} className='check' >
           {task.completed && <img className='size-5 w-full' src={Check} alt="Check" />}
         </button>
